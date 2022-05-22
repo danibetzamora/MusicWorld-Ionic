9ed61b19ba57555289de0f4f192e24b85938492a
danibetzamora <danielbetancor2000@gmail.com>
2022-05-22 18:30:41 +0100

    Added authentication to details page

--- src/app/details/details.page.ts
+++ src/app/details/details.page.ts
@@ -3,6 +3,7 @@ import { Gig } from "../services/gig.model";
 import { GigService} from "../services/gig.service";
 import { ActivatedRoute, ParamMap } from '@angular/router'
 import { SQLiteService } from '../services/SQLite.service';
+import {AuthenticateService} from "../services/authentication.service";
 
 @Component({
   selector: 'app-details',
@@ -13,12 +14,18 @@ export class DetailsPage implements OnInit {
 
   id: string = '';
   gig: Gig = new Gig;
+  private is_authenticated = null;
 
   constructor(
     private route: ActivatedRoute,
     private gigService: GigService,
     private sqlite: SQLiteService,
-  ) {}
+    private authenticationService: AuthenticateService,
+  ) {
+    this.authenticationService.authDetails().subscribe(
+      (user) => this.is_authenticated = (user != null)
+    )
+  }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe((params: ParamMap) => {
